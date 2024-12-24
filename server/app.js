cac866829c609456dd8a7cca3a33738bba124b9b
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2024-12-24 17:30:39 -0300

    .

--- server/app.js
+++ server/app.js
@@ -51,7 +51,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
 }));
 
