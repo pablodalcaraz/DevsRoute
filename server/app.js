3b23f51489cb1e866f8a003f83fa860e1c689a7c
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2024-12-26 19:50:22 -0300

    Condicional para archivos estáticos en producción

--- server/app.js
+++ server/app.js
@@ -14,11 +14,10 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 dotenv.config();
+
 // Definir __dirname utilizando import.meta.url
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -47,10 +46,6 @@ app.use(session({
   }
 }));
 
-app.use((req, res, next) => {
-  next();
-});
-
 app.use(cors({
   origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true,
@@ -58,10 +53,6 @@ app.use(cors({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
 
 app.use("/api", planRoutes);
 app.use("/api", registerRoutes);
@@ -71,10 +62,13 @@ app.use('/api', aprobadasRoutes);
 app.use('/api', alumnoRoutes);
 app.use('/api', equivalenciasRoutes);
 
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
+// Sirve archivos estáticos solo en producción
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en puerto ${port}`);
