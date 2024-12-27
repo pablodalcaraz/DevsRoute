cc3189ae8c914665eb6d2e2af5bb800390f0f844
Pablo Damián Alcaraz <pablodalcaraz@gmail.com>
2024-12-27 18:34:53 -0300

    Agrego app.use public

--- server/app.js
+++ server/app.js
@@ -64,6 +64,8 @@ app.use(express.json());
 
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../client/public')));
+
 
 
 app.use("/api", planRoutes);
